b184255c4f646a52acbe1420501862a9ba74bcb9
Tirtha050405 <tirtha.brahmbhatt5047@gmail.com>
2026-09-09 18:59:03 +0530

    Add Unit 3 Section 4 Q2

---
+++ Unit-03-Cursors/Section-4_Trace-Debug-Short-Answer/43_Section-4_Q2.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON;
+
+-- Error: PLS-00103 / invalid cursor parameter declaration.
+-- Correct line: CURSOR c_mem (p_course VARCHAR2) IS
+
+DECLARE
+    CURSOR c_mem (p_course VARCHAR2) IS
+        SELECT member_name FROM lib_member WHERE course = p_course;
+BEGIN
+    FOR r IN c_mem('MCA') LOOP
+        DBMS_OUTPUT.PUT_LINE(r.member_name);
+    END LOOP;
+END;
+/
